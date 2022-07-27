b2e77abf96629aac62f87cd429bb61f0cc155ce8
NgocTuIT99 <ngoctuvo493@gmail.com>
2022-07-27 17:16:48 +0700

    Day 14

---
+++ ReactJS/src/containers/HomePage/Section/About.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './About.scss';
+import Slider from 'react-slick';
+
+class About extends Component {
+
+    render() {
+
+        return (
+            <div className="section-About">
+                <div className="section-container">
+                    <div className="section-header">
+                        <span className="title-section">Cẩm nang</span>
+                        <button className="btn-section">Xem thêm</button>
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 2</div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 3</div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 4</div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 5</div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="bg-image" />
+                                <div>Cơ xương khớp 6</div>
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+// lay state cua redux
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
