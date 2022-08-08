dabf2e13fe70ed01244c03a4651ca924f5fff52c
NgocTuIT99 <ngoctuvo493@gmail.com>
2022-08-08 12:53:18 +0700

    Day 19

--- ReactJS/src/containers/System/Admin/UserRedux.js
+++ ReactJS/src/containers/System/Admin/UserRedux.js
@@ -2,90 +2,163 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { getAllCodeService } from '../../../services/userService';
+import * as actions from '../../../store/actions';
+import { LANGUAGES } from '../../../utils';
+import './UserRedux.scss';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 class UserRedux extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            genderArr: [],
+            positionArr: [],
+            roleArr: [],
+            previewImgURL: '',
+            isOpen: false
         }
     }
 
     async componentDidMount() {
-        try {
-            await getAllCodeService();
-        } catch (e) {
-            console.log(e);
+        this.props.getGenderStart();
+        this.props.getPositionStart();
+        this.props.getRoleStart();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.genderRedux !== this.props.genderRedux) {
+            this.setState({ genderArr: this.props.genderRedux });
+        }
+        if (prevProps.positionRedux !== this.props.positionRedux) {
+            this.setState({ positionArr: this.props.positionRedux });
+        }
+        if (prevProps.roleRedux !== this.props.roleRedux) {
+            this.setState({ roleArr: this.props.roleRedux });
         }
     }
 
+    handleOnChangeImage = (event) => {
+        let data = event.target.files;
+        let file = data[0];
+        if (file) {
+            let objUrl = URL.createObjectURL(file);
+            this.setState({
+                previewImgURL: objUrl
+            })
+        }
+    }
+
+    openPreviewImage = () => {
+        if (!this.state.previewImgURL) {
+            return;
+        }
+        this.setState({
+            isOpen: true
+        })
+    }
 
     render() {
+        let genders = this.state.genderArr;
+        let roles = this.state.roleArr;
+        let positions = this.state.positionArr;
+        let language = this.props.language;
+        let isLoading = this.props.isLoading;
         return (
             <div className="user-redux-container">
                 <div className="title">User Redux</div>
                 <div className="user-redux-body" >
                     <div className="container">
-                        <div class="row">
-                            <div class="col-3">
+                        <div className="row">
+                            <div className="col-12">{isLoading === true ? 'Loading data' : ''}</div>
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.email" /></label>
-                                <input type="email" class="form-control" />
+                                <input type="email" className="form-control" />
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.password" /></label>
-                                <input type="password" class="form-control" />
+                                <input type="password" className="form-control" />
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.first-name" /></label>
-                                <input type="text" class="form-control" />
+                                <input type="text" className="form-control" />
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.last-name" /></label>
-                                <input type="text" class="form-control" />
+                                <input type="text" className="form-control" />
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.phone-number" /></label>
-                                <input type="text" class="form-control" />
+                                <input type="text" className="form-control" />
                             </div>
-                            <div class="col-9">
+                            <div className="col-9">
                                 <label><FormattedMessage id="manage-user.address" /></label>
-                                <input type="text" class="form-control" />
+                                <input type="text" className="form-control" />
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.gender" /></label>
-                                <select class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                <select className="form-control">
+                                    {genders && genders.length > 0 &&
+                                        genders.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })}
                                 </select>
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.position" /></label>
-                                <select class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                <select className="form-control">
+                                    {positions && positions.length > 0 &&
+                                        positions.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })}
                                 </select>
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <label><FormattedMessage id="manage-user.role" /></label>
-                                <select class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                <select className="form-control">
+                                    {roles && roles.length > 0 &&
+                                        roles.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })}
                                 </select>
                             </div>
-                            <div class="col-3">
-                                <label for="inputState"><FormattedMessage id="manage-user.image" /></label>
-                                <input type="text" class="form-control" />
+                            <div className="col-3">
+                                <label><FormattedMessage id="manage-user.image" /></label>
+                                <div className="preview-img-container">
+                                    <input id="previewImg" type="file" hidden
+                                        onChange={(event) => this.handleOnChangeImage(event)}
+                                    />
+                                    <label className="lable-upload" htmlFor="previewImg">Tải ảnh <i className="fas fa-upload"></i></label>
+                                    <div className="preview-image"
+                                        style={{ backgroundImage: `url(${this.state.previewImgURL})` }}
+                                        onClick={() => this.openPreviewImage()}
+                                    ></div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-12 mt-3">
-                            <button type="submit" class="btn btn-primary"><FormattedMessage id="manage-user.save" /></button>
+                            <div className="col-12 mt-3">
+                                <button type="submit" className="btn btn-primary"><FormattedMessage id="manage-user.save" /></button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {this.state.isOpen === true &&
+                    <Lightbox
+                        mainSrc={this.state.previewImgURL}
+                        onCloseRequest={() => this.setState({ isOpen: false })}
+                    />
+                }
             </div>
         )
     }
@@ -94,11 +167,19 @@ class UserRedux extends Component {
 
 const mapStateToProps = state => {
     return {
+        language: state.app.language,
+        genderRedux: state.admin.genders,
+        positionRedux: state.admin.positions,
+        roleRedux: state.admin.roles,
+        isLoading: state.admin.isLoading,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        getGenderStart: () => dispatch(actions.fetchGenderStart()),
+        getPositionStart: () => dispatch(actions.fetchPositionStart()),
+        getRoleStart: () => dispatch(actions.fetchRoleStart())
     };
 };
 
--- ReactJS/src/store/reducers/adminReducer.js
+++ ReactJS/src/store/reducers/adminReducer.js
@@ -1,33 +1,68 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLoggedIn: false,
-    adminInfo: null
+    isLoading: false,
+    genders: [],
+    roles: [],
+    positions: [],
 }
 
-const appReducer = (state = initialState, action) => {
+const adminReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADMIN_LOGIN_SUCCESS:
+        case actionTypes.FETCH_GENDER_START:
+            state.isLoading = true;
             return {
                 ...state,
-                isLoggedIn: true,
-                adminInfo: action.adminInfo
             }
-        case actionTypes.ADMIN_LOGIN_FAIL:
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data;
+            state.isLoading = false;
             return {
                 ...state,
-                isLoggedIn: false,
-                adminInfo: null
             }
-        case actionTypes.PROCESS_LOGOUT:
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoading = false;
+            state.genders = [];
             return {
                 ...state,
-                isLoggedIn: false,
-                adminInfo: null
+            }
+        case actionTypes.FETCH_POSITION_START:
+            state.isLoading = true;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            state.isLoading = false;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.isLoading = false;
+            state.positions = [];
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_ROLE_START:
+            state.isLoading = true;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            state.isLoading = false;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.isLoading = false;
+            state.roles = [];
+            return {
+                ...state,
             }
         default:
             return state;
     }
 }
 
-export default appReducer;
+export default adminReducer;
